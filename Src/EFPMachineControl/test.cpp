92c081c0d066ecd98982f24b593b9ae13d208d52
KuraZuzu <1ceim020@mail.u-tokai.ac.jp>
2022-11-19 04:58:38 +0900

    adcのサイクルは分周なので大きい方が放電時間を確保できるらしい

--- Src/EFPMachineControl/test.cpp
+++ Src/EFPMachineControl/test.cpp
@@ -72,37 +72,33 @@ void Test::batteryConsoleDebug() {
 }
 
 void Test::DistanceSensorConsoleDebug() {
-    uint32_t lf=0, ls=0, rs=0, rf=0;
-//    std::vector<uint16_t> lf_values(100, 0);
-//    std::vector<uint16_t> ls_values(100, 0);
-//    std::vector<uint16_t> rs_values(100, 0);
-//    std::vector<uint16_t> rf_values(100, 0);
-//    for (int i = 0; i < 100; ++i) {
-//        lf_values[i] = _lf_sensor.getTestRawValue();
-//        ls_values[i] = _ls_sensor.getTestRawValue();
-//        rs_values[i] = _rs_sensor.getTestRawValue();
-//        rf_values[i] = _rf_sensor.getTestRawValue();
-//        HAL_Delay(10);
-//    }
-//    std::sort(lf_values.begin(), lf_values.end());
-//    std::sort(ls_values.begin(), ls_values.end());
-//    std::sort(rs_values.begin(), rs_values.end());
-//    std::sort(rf_values.begin(), rf_values.end());
-//    lf = lf_values[50];
-//    ls = ls_values[50];
-//    rs = rs_values[50];
-//    rf = rf_values[50];
-    lf = _lf_sensor.getTestRawValue();
-    ls = _ls_sensor.getTestRawValue();
-    rs = _rs_sensor.getTestRawValue();
-    rf = _rf_sensor.getTestRawValue();
+    float32_t lf=0, ls=0, rs=0, rf=0;
+    std::vector<float32_t> lf_values(100, 0);
+    std::vector<float32_t> ls_values(100, 0);
+    std::vector<float32_t> rs_values(100, 0);
+    std::vector<float32_t> rf_values(100, 0);
+    for (int i = 0; i < 100; ++i) {
+        lf_values[i] = _lf_sensor.getTestRawValue();
+        ls_values[i] = _ls_sensor.getTestRawValue();
+        rs_values[i] = _rs_sensor.getTestRawValue();
+        rf_values[i] = _rf_sensor.getTestRawValue();
+        HAL_Delay(10);
+    }
+    std::sort(lf_values.begin(), lf_values.end());
+    std::sort(ls_values.begin(), ls_values.end());
+    std::sort(rs_values.begin(), rs_values.end());
+    std::sort(rf_values.begin(), rf_values.end());
+    lf = lf_values[50];
+    ls = ls_values[50];
+    rs = rs_values[50];
+    rf = rf_values[50];
 
-    printf("LF: %d   LS: %d  RS: %d  RF: %d\r\n"
-            , static_cast<int>(lf)
-            , static_cast<int>(ls)
-            , static_cast<int>(rs)
-            , static_cast<int>(rf)
-    );
+//    lf = _lf_sensor.getTestRawValue();
+//    ls = _ls_sensor.getTestRawValue();
+//    rs = _rs_sensor.getTestRawValue();
+//    rf = _rf_sensor.getTestRawValue();
+
+    printf("LF: %f   LS: %f  RS: %f  RF: %f\r\n", lf, ls, rs, rf);
     HAL_Delay(10);
 }
 
--- Src/adc.c
+++ Src/adc.c
@@ -62,7 +62,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_8;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_480CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -79,7 +79,6 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_11;
   sConfig.Rank = 3;
-  sConfig.SamplingTime = ADC_SAMPLETIME_144CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -88,7 +87,6 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_12;
   sConfig.Rank = 4;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
